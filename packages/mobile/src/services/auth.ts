1159e0ba89bf99f3ca272565282daa4bf99376e2
daniel-drozdzel <daniel.drozdzel@vattenfall.com>
2025-04-28 22:23:17 +0200

    fix to working version

--- packages/mobile/src/services/auth.ts
+++ packages/mobile/src/services/auth.ts
@@ -5,7 +5,7 @@ import { Platform } from 'react-native';
 export const API_URL =
   Platform.select({
     ios: 'http://localhost:8000',
-    android: 'http://10.0.2.2:8000', // Standardowy adres hosta dla emulatora Android
+    android: 'http://192.168.0.112:8000',
   }) || 'http://localhost:8000';
 
 // Klucze dla AsyncStorage
